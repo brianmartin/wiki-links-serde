8cded11296175c0e4cebd07ed633f51f50038c65
Brian Martin <brianmartin@gmail.com>
2012-10-17 13:08:25 -0400

    add implicit for bytebuffer -> string

--- src/main/scala/com/github/brianmartin/wiki/Runner.scala
+++ src/main/scala/com/github/brianmartin/wiki/Runner.scala
@@ -1,49 +1,24 @@
 package com.github.brianmartin.wiki
 
-import kba._
+import kba.wiki._
+
 import java.io.File
+import java.nio.ByteBuffer
+import java.nio.charset.CharsetDecoder
+import java.nio.charset.Charset
+
 import scala.io.Source
 
+object implicits {
+  implicit def string2bytebuffer(str: String): ByteBuffer = ByteBuffer.wrap(str.getBytes())
+  implicit def bytes2bytebuffer(bytes: Array[Byte]): ByteBuffer = ByteBuffer.wrap(bytes)
+}
+
 object Runner {
-
+  
+  import implicits._
+  
   def main(args: Array[String]): Unit = {
-
-    val s = StreamTime(
-        epochTicks = 1,
-        zuluTimestamp = "hello"
-      )
-    
-//    val s = StreamItem(
-//        docId = "1",
-//        absUrl= null,
-//        schost = null,
-//        originalUrl = null,
-//        source = null,
-//        title = null,
-//        body = null,
-//        anchor = null,
-//        sourceMetadata = null,
-//        streamId = null,
-//        streamTime = null,
-//        annotation = null
-//      )
-
-    
-    val file = new File("/Users/brian/wrk-umass/wiki-links-thrift/thrifting1/test.out")
-    val (outStream, outProto) = ThriftSerializerFactory.getWriter(file)
-    
-    s.write(outProto)
-    
-    outStream.flush()
-    outStream.close()
-    
-    val (inStream, inProto) = ThriftSerializerFactory.getReader(file)
-    
-    val time = StreamTime.decode(inProto)
-    println(time.epochTicks)
-    println(time.zuluTimestamp)
-    
-    inStream.close()
     
     val htmlFile = new File("/Users/brian/wrk-umass/wiki-links-thrift/wiki-link-chunk-000000/pages/000000/377")
     val htmlStr  = Source.fromFile(htmlFile).getLines().mkString("\n")
