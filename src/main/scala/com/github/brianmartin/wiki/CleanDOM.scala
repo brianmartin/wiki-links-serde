34b131051c23ee7134a8f1f71206c1be5e942f83
Brian Martin <brianmartin@gmail.com>
2012-10-17 14:24:20 -0400

    CleanDOM takes raw HTML and returns an Option

--- src/main/scala/com/github/brianmartin/wiki/CleanDOM.scala
+++ src/main/scala/com/github/brianmartin/wiki/CleanDOM.scala
@@ -3,23 +3,32 @@ import scala.xml.PrettyPrinter
 import scala.xml.XML
 import scala.io.Source
 import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+import java.io.BufferedReader
+import java.io.StringReader
 
 object CleanDOM {
   
-  def apply(filename: String): String = {
-    
-    // instantiate the parser
-    val parser = XML.withSAXParser((new SAXFactoryImpl).newSAXParser())
-    val pp = new PrettyPrinter(300, 2)
-    
-    // parse the file
-    val htmlReader = Source.fromFile(filename)("UTF-8").bufferedReader()
-    val html = parser.load(htmlReader)
-    
-    // pretty-print the DOM
-    val sb = new StringBuilder
-    pp.format(html, sb)
-    sb.toString()
+  def apply(rawHTML: String): Option[String] = {
+    try {
+	    
+	    // instantiate the parser
+	    val parser = XML.withSAXParser((new SAXFactoryImpl).newSAXParser())
+	    val pp = new PrettyPrinter(300, 2)
+	    
+	    // parse the file
+	    val htmlReader = new BufferedReader(new StringReader(rawHTML))
+	    val html = parser.load(htmlReader)
+	    
+	    // pretty-print the DOM
+	    val sb = new StringBuilder
+	    pp.format(html, sb)
+	    val res = Some(sb.toString())
+	    println(res)
+	    res
+	    
+    } catch {
+	   case _ => None
+	}
   }
 
 }
