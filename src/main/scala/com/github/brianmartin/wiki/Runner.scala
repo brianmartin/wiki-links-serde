abc5faa45882a130f6f66d5e14e95b188cdc433f
Brian Martin <brianmartin@gmail.com>
2012-10-17 13:42:59 -0400

    remove cruft from Runner

--- src/main/scala/com/github/brianmartin/wiki/Runner.scala
+++ src/main/scala/com/github/brianmartin/wiki/Runner.scala
@@ -2,12 +2,7 @@ package com.github.brianmartin.wiki
 
 import kba.wiki._
 
-import java.io.File
 import java.nio.ByteBuffer
-import java.nio.charset.CharsetDecoder
-import java.nio.charset.Charset
-
-import scala.io.Source
 
 object implicits {
   implicit def string2bytebuffer(str: String): ByteBuffer = ByteBuffer.wrap(str.getBytes())
@@ -16,14 +11,9 @@ object implicits {
 
 object Runner {
   
-  import implicits._
-  
   def main(args: Array[String]): Unit = {
     
-    val htmlFile = new File("/Users/brian/wrk-umass/wiki-links-thrift/wiki-link-chunk-000000/pages/000000/377")
-    val htmlStr  = Source.fromFile(htmlFile).getLines().mkString("\n")
-    
-    println(CleanseHTML(htmlStr))
+    val htmlPath = "/Users/brian/wrk-umass/wiki-links-thrift/wiki-link-chunk-000000/pages/000000/377"
 
   }
 
