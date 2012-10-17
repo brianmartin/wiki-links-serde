f77f7aee767219539428e3eca90d534d7cfa9662
Brian Martin <brianmartin@gmail.com>
2012-10-17 13:08:00 -0400

    add deps for tagsoup, guava, scalatest

--- project/Build.scala
+++ project/Build.scala
@@ -21,15 +21,25 @@ object Thrifting1Build extends Build {
           "boilerpipe-m2-repo" at "https://boilerpipe.googlecode.com/svn/repo/"
         ),
         libraryDependencies ++= Seq(
+          // Thrift
           "org.apache.thrift" % "libthrift" % "0.8.0",
           "org.slf4j" % "slf4j-api" % "1.6.6",
           "com.twitter" % "finagle-core" % finagleVersion,
           "com.twitter" % "finagle-thrift" % finagleVersion,
           "org.jboss.netty" % "netty" % "3.2.6.Final",
           "com.twitter" %% "scrooge-runtime" % "3.0.1",
+          // Boilerpipe
           "xerces" % "xercesImpl" % "2.9.1",
           "net.sourceforge.nekohtml" % "nekohtml" % "1.9.13",
-          "de.l3s.boilerpipe" % "boilerpipe" % "1.2.0"
+          "de.l3s.boilerpipe" % "boilerpipe" % "1.2.0",
+          // Tagsoup
+          "org.ccil.cowan.tagsoup" % "tagsoup" % "1.2",
+          // JSON
+          "net.liftweb" %% "lift-json" % "2.5-M1",
+          // Guava
+          "com.google.guava" % "guava" % "13.0",
+          // Testing
+          "org.scalatest" %% "scalatest" % "1.8" % "test"
         ),
         publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
       )
