c197dc28bc12726dc5e94e851780a4cd9eb25e0a
Brian Martin <brianmartin@gmail.com>
2012-10-17 11:53:54 -0400

    add boilerpipe dependency

--- project/Build.scala
+++ project/Build.scala
@@ -16,14 +16,20 @@ object Thrifting1Build extends Build {
         organization := "com.github.brianmartin",
         version := "0.1-SNAPSHOT",
         scalaVersion := "2.9.2",
-        resolvers += "twitter-repo" at "http://maven.twttr.com",
+        resolvers ++= Seq(
+          "twitter-repo" at "http://maven.twttr.com",
+          "boilerpipe-m2-repo" at "https://boilerpipe.googlecode.com/svn/repo/"
+        ),
         libraryDependencies ++= Seq(
           "org.apache.thrift" % "libthrift" % "0.8.0",
           "org.slf4j" % "slf4j-api" % "1.6.6",
           "com.twitter" % "finagle-core" % finagleVersion,
           "com.twitter" % "finagle-thrift" % finagleVersion,
           "org.jboss.netty" % "netty" % "3.2.6.Final",
-          "com.twitter" %% "scrooge-runtime" % "3.0.1"
+          "com.twitter" %% "scrooge-runtime" % "3.0.1",
+          "xerces" % "xercesImpl" % "2.9.1",
+          "net.sourceforge.nekohtml" % "nekohtml" % "1.9.13",
+          "de.l3s.boilerpipe" % "boilerpipe" % "1.2.0"
         ),
         publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
       )
