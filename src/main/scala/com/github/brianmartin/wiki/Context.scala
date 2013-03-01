3ff047a8b33b69f2e37544214558a9dbf8988ed5
Harshal Pandya <harshalgem@gmail.com>
2013-03-01 14:17:09 -0500

    add wikipedia to freebase maps add comments

--- src/main/scala/com/github/brianmartin/wiki/Context.scala
+++ src/main/scala/com/github/brianmartin/wiki/Context.scala
@@ -1,12 +1,13 @@
 package com.github.brianmartin.wiki
 
 import org.jsoup.nodes.Element
-import edu.umass.cs.iesl.wiki.{Mention, Context=>ContextWrapper}
+import edu.umass.cs.iesl.wiki.{Context => ContextWrapper, WikiLinkItem, Mention}
 import java.util.regex.Pattern
 import org.jsoup.Jsoup
 import collection.mutable
-import collection.mutable.ArrayBuffer
+import collection.mutable.{HashMap, ArrayBuffer}
 import org.apache.commons.lang.StringUtils
+import io.Source
 
 /**
  * @author harshal
@@ -14,6 +15,33 @@ import org.apache.commons.lang.StringUtils
  */
 object Context {
   val window_size = 15
+  val redirectsMap = loadRedirectsMap("/iesl/canvas/harshal/data/wikipedia/extraction/redirects/redirects_final.tsv")
+  val title2id = loadTitleIdMap("/iesl/canvas/harshal/data/wikipedia/wpid-title-mapping-new.csv")
+  val wiki2fb = loadWikiToFreebaseMap("/iesl/canvas/harshal/data/freebase/freebase-wex-2012-08-06-freebase_wpid.tsv")
+
+  /**
+   * takes a wikilinkitem and returns a seq[Mention] with context and freebaseid filled in
+   * @param wli
+   * @return
+   */
+
+  def addContextAndFreebaseId(wli:WikiLinkItem):Seq[Mention]={
+    def newMentionArray = ArrayBuffer[Mention]()
+
+    val _map = mutable.HashMap[String,ArrayBuffer[Mention]]()
+    wli.mentions.foreach(mention=>{
+      _map(pruneUrl(mention._1)) = (_map.getOrElseUpdate(pruneUrl(mention._1),newMentionArray)+=mention)
+    })
+
+    wli.content.dom match {
+      case Some(dom) => {
+        val mentions = processDom(dom,_map,wli.docId)
+        mentions
+      }
+      case None => wli.mentions
+    }
+  }
+
   /**
    * Recursively find a large enough context for the anchor by looking at the parents
    * @param anchor
@@ -39,9 +67,10 @@ object Context {
   }
 
   def processDom(html:String,_map:mutable.HashMap[String,ArrayBuffer[Mention]],id:Int)
-  :(Seq[Mention],Int) = {
+  :Seq[Mention] = {
     val Matcher = """.+/(.*)""".r
     val doc = Jsoup.parse(html)
+    //Get all the anchor elements from the dom to compare with wikiurl
     val anchors = doc.getElementsByTag("a").iterator()
     val mentions = ArrayBuffer[Mention]()
     while(!_map.isEmpty && anchors.hasNext){
@@ -51,41 +80,72 @@ object Context {
       if(!prunedHref.equals("")){ //Check if its a wikipedia url
         if (_map.contains(prunedHref)){
           val value = _map.get(prunedHref).get
+          /**
+           * there could be multiple links for the same url in the page
+           * this is stored as a list in the corresponding map bucket
+           * hence when a url is matched it should be removed list so it's not matched twice
+           * when the array is empty remove corresponding map entry
+           */
           val mention = value.remove(0)
           if(value.length==0) _map.remove(prunedHref)
           else _map(prunedHref) = value
+          //some urls end with "/" which should be removed
           val wikiUrl = StringUtils.stripEnd(mention.wikiUrl,"/")
+          //extract wiki page title from the url
           val title = wikiUrl match {
             case Matcher(title) => title
             case _ => {
               """(?<=http://en.wikipedia.org/wiki/)(.*)(?!/)""".r.findFirstIn(wikiUrl).get
             }
           }
-          //TODO remove comments
-          //val idOp = title2id.get(StringUtils.stripEnd(title.toLowerCase,"_"))
+          //get the wikipedia id for the title
+          val idOp = title2id.get(StringUtils.stripEnd(title.toLowerCase,"_"))
+          //generate a mention with context and fb id filed in
           mentions+= Mention(mention.wikiUrl, mention.anchorText,mention.rawTextOffset,
             Some(getContext(anchor,anchor.parent())),
-            None
-//            idOp match {
-//              case Some(id) => wiki2fb.get(id)
-//              case None => None
-//            }
+            idOp match {
+              case Some(id) => wiki2fb.get(redirectsMap.getOrElse(id,id))
+              case None => None
+            }
           )
         }
       }
     }
-
-    val status = if (mentions.size==0) 1 else 0
-
+    //add the mentions not found in page back with empty context and freebase id
     mentions++=_map.values.flatten
-
-    (mentions,status)
   }
 
   // pruning is necessary because sites might garbled urls like: <a href="//en.wikipedia...."> (that is, no "http:")
   def pruneUrl(url: String): String = {
-    val index = url.indexOf("wikipedia.org");
+    val index = url.indexOf("wikipedia.org")
     if(index != -1) url.substring(index) else ""
   }
 
+  def loadWikiToFreebaseMap(wikiToFreeBaseFile:String) : HashMap[String,String] ={
+    println("Loading from "+wikiToFreeBaseFile+"...")
+    val map = HashMap[String,String]()
+    for(line<-Source.fromFile(wikiToFreeBaseFile).getLines()){
+      val split = line.split("\t")
+      if(split.size==2) map(split(1))=split(0)
+    }
+    map
+  }
+  def loadTitleIdMap(titleIdFile:String) : HashMap[String,String]={
+    println("Loading from "+titleIdFile+"...")
+    val map = HashMap[String,String]()
+    for(line<-Source.fromFile(titleIdFile).getLines()){
+      val split = line.split("\t")
+      if(split.size ==2) map(split(0).toLowerCase) = split(1)
+    }
+    map
+  }
+  def loadRedirectsMap(file:String) : HashMap[String,String] = {
+    println("Loading from "+file+"...")
+    val map = HashMap[String,String]()
+    for(line<-Source.fromFile(file).getLines()){
+      val split = line.split("\t")
+      if(split.size ==2) map(split(0)) = split(1)
+    }
+    map
+  }
 }
