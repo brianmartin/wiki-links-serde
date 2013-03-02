a26d261328525a51cfb348bcf83ab64385bb25d3
Harshal Pandya <harshalgem@gmail.com>
2013-03-01 19:57:07 -0500

    Tested; Fixed "take" bug

--- src/main/scala/com/github/brianmartin/wiki/Context.scala
+++ src/main/scala/com/github/brianmartin/wiki/Context.scala
@@ -1,7 +1,7 @@
 package com.github.brianmartin.wiki
 
 import org.jsoup.nodes.Element
-import edu.umass.cs.iesl.wiki.{Context => ContextWrapper, WikiLinkItem, Mention}
+import edu.umass.cs.iesl.wiki.{Context => ContextWrapper, PageContentItem, WikiLinkItem, Mention}
 import java.util.regex.Pattern
 import org.jsoup.Jsoup
 import collection.mutable
@@ -15,7 +15,7 @@ import io.Source
  */
 object Context {
   val window_size = 15
-  val redirectsMap = loadRedirectsMap("/iesl/canvas/harshal/data/wikipedia/extraction/redirects/redirects_final.tsv")
+  val redirectsMap =  loadRedirectsMap("/iesl/canvas/harshal/data/wikipedia/extraction/redirects/redirects_final.tsv")
   val title2id = loadTitleIdMap("/iesl/canvas/harshal/data/wikipedia/wpid-title-mapping-new.csv")
   val wiki2fb = loadWikiToFreebaseMap("/iesl/canvas/harshal/data/freebase/freebase-wex-2012-08-06-freebase_wpid.tsv")
 
@@ -28,10 +28,10 @@ object Context {
   def addContextAndFreebaseId(wli:WikiLinkItem):WikiLinkItem={
     def newMentionArray = ArrayBuffer[Mention]()
 
-    val _map = mutable.HashMap[String,(ArrayBuffer[Mention],Int)]()
+    val _map = mutable.HashMap[(String,String),(ArrayBuffer[Mention],Int)]()
     wli.mentions.foreach(mention=>{
-      val res = _map.getOrElseUpdate(pruneUrl(mention._1),(newMentionArray,0))
-      _map(pruneUrl(mention._1)) = Tuple2(res._1+=mention,res._2+1)
+      val res = _map.getOrElseUpdate((pruneUrl(mention._1),mention.`anchorText`),(newMentionArray,0))
+      _map((pruneUrl(mention._1),mention.`anchorText`)) = Tuple2(res._1+=mention,res._2+1)
     })
 
     wli.content.dom match {
@@ -63,7 +63,7 @@ object Context {
       var split = left.split("""\s+""")
       val leftStr = if (split.length<=window_size) left else split.takeRight(window_size).mkString(" ")
       split = right.split("""\s+""")
-      val rightStr = if (split.length<=window_size) right else split.takeRight(window_size).mkString(" ")
+      val rightStr = if (split.length<=window_size) right else split.take(window_size).mkString(" ")
       return ContextWrapper(leftStr,rightStr,anchor.text())
     }
     else{
@@ -71,7 +71,7 @@ object Context {
     }
   }
 
-  def processDom(html:String,_map:mutable.HashMap[String,(ArrayBuffer[Mention],Int)],id:Int)
+  def processDom(html:String,_map:mutable.HashMap[(String,String),(ArrayBuffer[Mention],Int)],id:Int)
   :Seq[Mention] = {
     val Matcher = """.+/(.*)""".r
     val doc = Jsoup.parse(html)
@@ -80,11 +80,12 @@ object Context {
     val mentions = ArrayBuffer[Mention]()
     while(!_map.isEmpty && anchors.hasNext){
       val anchor = anchors.next()
+      val anchorText = anchor.text().trim
       val href = anchor.attr("href")
       val prunedHref = pruneUrl(href)
       if(!prunedHref.equals("")){ //Check if its a wikipedia url
-        if (_map.contains(prunedHref)){
-          val value = _map.get(prunedHref).get
+        if (_map.contains((prunedHref,anchorText))){
+          val value = _map.get((prunedHref,anchorText)).get
           /**
            * there could be multiple links for the same url in the page
            * this is stored as a list in the corresponding map bucket
@@ -92,8 +93,8 @@ object Context {
            * when the array is empty remove corresponding map entry
            */
           val mention = value._1.remove(0)
-          if(value._1.length==0) _map.remove(prunedHref)
-          else _map(prunedHref) = value
+          if(value._1.length==0) _map.remove((prunedHref,anchorText))
+          else _map((prunedHref,anchorText)) = value
           //Implicit value to be passed to getContext
           implicit val index_within_map_array = value._2-value._1.length
           //some urls end with "/" which should be removed
@@ -155,4 +156,45 @@ object Context {
     }
     map
   }
+
+  /**
+   * Test case, uncomment and run
+    */
+//  def main(args:Array[String]){
+//    val wli = edu.umass.cs.iesl.wiki.WikiLinkItem(
+//      docId = 1,
+//      url = "",
+//      content = PageContentItem(
+//        raw = null,
+//        fullText = null,
+//        articleText = null,
+//        dom = Some("""<!DOCTYPE html><html><head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# githubog: http://ogp.me/ns/fb/githubog#"><title></title></head>"""+
+//                  """<body class="logged_in page-profile macintosh  env-production  "><div id="wrapper"><p><a href="http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy">Baron Augustin-Louis Cauchy</a> was a French mathematician who was an early pioneer of analysis. He started the project of formulating and proving the theorems of infinitesimal calculus in a rigorous manner, rejecting the heuristic principle of the generality of algebra exploited by earlier authors.</p>"""+
+//                  """<p>"More concepts and theorems have been named for Cauchy than for any other mathematician (in elasticity alone there are sixteen concepts and theorems named for <a href="http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy">Cauchy</a>)."</p>"""+
+//                    """<p>Cauchy was a prolific writer; he wrote approximately eight hundred research articles and five complete textbooks. He was a devout Roman Catholic, strict Bourbon royalist, and a close associate of the Jesuit order.</p>"""+
+//                    """<p><a href="http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy"> Cauchy </a> was the son of Louis François Cauchy (1760–1848) and Marie-Madeleine Desestre. Cauchy had two brothers, Alexandre Laurent <a href="http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy"> Cauchy </a> (1792–1857), who became a president of a division of the court of appeal in 1847, and a judge of the court of cassation in 1849; and Eugene François Cauchy (1802–1877), a publicist who also wrote several mathematical works."""+
+//                """Cauchy married Aloise de Bure in 1818. She was a close relative of the publisher who published most of Cauchy's works. By her he had two daughters, Marie Françoise Alicia (1819) and Marie Mathilde (1823).</p>"""+
+//                    """</div></body></html>""")
+//      ),
+//      rareWords = null,
+//      mentions = Seq(Mention(
+//      "http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy",
+//      "Baron Augustin-Louis Cauchy",
+//      0),
+//        Mention(
+//          "http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy",
+//          "Cauchy",
+//          0),
+//        Mention(
+//          "http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy",
+//          "Cauchy",
+//          0),
+//        Mention(
+//          "http://en.wikipedia.org/wiki/Augustin-Louis_Cauchy",
+//          "Cauchy",
+//          0))
+//    )
+//    addContextAndFreebaseId(wli).`mentions`.foreach(println(_))
+//
+//  }
 }
