bb7e2f50d3879bb9e02c4a1e448349990a0827e2
Brian Martin <brianmartin@gmail.com>
2013-04-17 16:15:58 -0400

    add WikiLinkItemIterator (for multiple in the same file)

--- src/main/scala/com/github/brianmartin/wiki/Viewer.scala
+++ src/main/scala/com/github/brianmartin/wiki/Viewer.scala
@@ -28,3 +28,24 @@ object Viewer {
   }
 
 }
+
+class WikiLinkItemIterator(f: File) extends Iterator[WikiLinkItem] {
+  
+  import org.apache.thrift.transport.TTransportException
+
+  var done = false
+  val (stream, proto) = ThriftSerializerFactory.getReader(f)
+  private var _next: Option[WikiLinkItem] = getNext()
+
+  private def getNext(): Option[WikiLinkItem] = try { 
+    Some(WikiLinkItem.decode(proto))
+  } catch { case _: TTransportException => { done = true; stream.close(); None }}
+
+  def hasNext(): Boolean = !done && (_next != None || { _next = getNext(); _next != None })
+
+  def next(): WikiLinkItem = if (hasNext()) _next match {
+    case Some(wli) => { _next = None; wli }
+    case None => { throw new Exception("Next on empty iterator.") }
+  } else throw new Exception("Next on empty iterator.")
+
+}
